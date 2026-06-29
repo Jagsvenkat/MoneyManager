99bb3591fc05368868e345adb70a5085a2e8eeb6
jagsvenkat <jagssparrow3@gmail.com>
2026-06-29 15:45:21 +0530

    Remove dead code and unrelated files (old screens, deprecated services, docs, AI config)

--- lib/core/models/models.dart
+++
@@ -1,4 +0,0 @@
-// Core data models for Money Manager
-// NOTE: Hive type adapters were removed because this file is unused.
-// The app stores all data as encrypted envelope JSON strings in Hive boxes.
-// If you need structured Hive models, run: dart run build_runner build
